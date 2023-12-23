2831eeb3d00ce0d5b53dde7ed59694d2170f76c6
JereUR <jeremias.jdv@gmail.com>
2023-12-23 18:49:10 -0300

    feat(Root): Routes for tasks page and form created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import TasksPage from './pages/TasksPage'
+import TaskFormPage from './pages/TaskFormPage'
+
 export default function App() {
-  return <div>Hello world</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/tasks" element={<TasksPage />} />
+        <Route path="/tasks-create" element={<TaskFormPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
