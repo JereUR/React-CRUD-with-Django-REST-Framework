c3c6b4c226403cc64fad9b8ad0dbbe2c3d0992d7
JereUR <jeremias.jdv@gmail.com>
2023-12-22 22:14:45 -0300

    feat(Root): Api documentation added

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'coreapi',
     'tasks'
 ]
 
@@ -129,3 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS authorization
 CORS_ALLOWED_ORIGINS = [
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+}
