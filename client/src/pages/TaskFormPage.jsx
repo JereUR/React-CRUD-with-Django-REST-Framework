00b8c774320f228c964c41951451d4aece551cb9
JereUR <jeremias.jdv@gmail.com>
2023-12-26 21:27:21 -0300

    feat(Root): Create tasks form and action added

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,3 +1,47 @@
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+
+import { createTask } from '../api/tasks.api'
+
 export default function TaskFormPage() {
-  return <div>TaskFormPage</div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
+
+  const navigate = useNavigate()
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      const res = await createTask(data)
+      if (res.status === 201) {
+        navigate('/tasks')
+      } else {
+        console.error('Error en la solicitud:', res.status, res.statusText)
+      }
+    } catch (error) {
+      console.error('Error en la solicitud:', error)
+    }
+  })
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          {...register('title', { required: true })}
+        />
+        {errors.title && <span>Title is required.</span>}
+        <textarea
+          rows="3"
+          placeholder="Description"
+          {...register('description', { required: true })}
+        ></textarea>
+        {errors.description && <span>Description is required.</span>}
+        <button>Save</button>
+      </form>
+    </div>
+  )
 }
