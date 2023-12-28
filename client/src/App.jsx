e4945203cb9c926597294a1ed1e06b7a97656430
JereUR <jeremias.jdv@gmail.com>
2023-12-27 21:41:31 -0300

    feat(Root): Toaster component for notifications added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 import TasksPage from './pages/TasksPage'
 import TaskFormPage from './pages/TaskFormPage'
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/tasks-create" element={<TaskFormPage />} />
         <Route path="/tasks/:id" element={<TaskFormPage />} />
       </Routes>
+      <Toaster />
     </BrowserRouter>
   )
 }
